0ed54505c144f9e53fe054bf516e80efd94d7a9b
Daksh <dakshjain48@gmail.com>
2019-07-28 19:24:13 +0530

    frontend added

---
+++ build/precache-manifest.255c31ded84b8505684ae90017289365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63dfb9ce8ceaddbd37d6e5b8fe0f6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cb2306a4c5876571684",
+    "url": "/static/css/main.26af05b9.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "1cb2306a4c5876571684",
+    "url": "/static/js/main.db9383f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
